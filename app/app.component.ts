df73ae6d6a3d116bf63b86a979167bff7e4bcb8d
Neil-Ni <tzuyangni@gmail.com>
2016-06-14 23:10:02 -0400

    add progressbar section

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { AlertDemoComponent }      from './components/alert/alert-demo';
-import { ButtonsDemoComponent }    from './components/buttons/buttons-demo';
-import { PaginationDemoComponent } from './components/pagination/pagination-demo';
-import { SectionComponent }        from './components/section/section.component';
+import { AlertDemoComponent }       from './components/alert/alert-demo';
+import { ButtonsDemoComponent }     from './components/buttons/buttons-demo';
+import { PaginationDemoComponent }  from './components/pagination/pagination-demo';
+import { ProgressbarDemoComponent } from './components/progressbar/progressbar-demo';
+import { SectionComponent }         from './components/section/section.component';
 
 @Component({
   selector: 'my-app',
   directives: [
-    AlertDemoComponent, ButtonsDemoComponent, SectionComponent, PaginationDemoComponent
+    AlertDemoComponent,
+    ButtonsDemoComponent,
+    SectionComponent,
+    PaginationDemoComponent,
+    ProgressbarDemoComponent
   ],
   templateUrl: 'app/app.component.html'
 })
